3aa95b37acbf6818466c0317316401d4f2c9b78c
Shane Abe <sabe@intellective.com>
2020-10-22 15:28:35 -0400

    Item 2 from email - Only layout changes; still waiting for JIRA issue collector change

---
+++ assets/js/contact-us.js
@@ -0,0 +1,27 @@
+
+function enhancementRequest() {
+    // jQuery.ajax({
+    //     url: "https://jira.intellective.com/rest/collectors/1.0/template/form/ebed4e32",
+    //     type: "get",
+    //     cache: true,
+    //     dataType: "script"
+    // });    
+}
+
+function requestInfo() {
+    // jQuery.ajax({
+    //     url: "https://jira.intellective.com/s/b3827f4461d4436c6714c921c20d1f97-T/-mb7qmt/810001/ed30881a1c0940ba1231758ef6f853f8/4.0.1/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=c98c0312",
+    //     type: "get",
+    //     cache: true,
+    //     dataType: "script"
+    // });
+}
+
+function reportBug() {
+    // jQuery.ajax({
+    //     url: "https://jira.intellective.com/s/b3827f4461d4436c6714c921c20d1f97-T/-mb7qmt/810001/ed30881a1c0940ba1231758ef6f853f8/4.0.1/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=7a36e89a",
+    //     type: "get",
+    //     cache: true,
+    //     dataType: "script"
+    // });
+}
