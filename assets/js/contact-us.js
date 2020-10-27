e6d40260eb156502887ff9bf7cbeea0730f7b4c5
Shane Abe <sabe@intellective.com>
2020-10-27 13:02:05 -0400

    add JIRA issue collector functionality

--- assets/js/contact-us.js
+++ assets/js/contact-us.js
@@ -1,27 +1,27 @@
 
-function enhancementRequest() {
-    // jQuery.ajax({
-    //     url: "https://jira.intellective.com/rest/collectors/1.0/template/form/ebed4e32",
-    //     type: "get",
-    //     cache: true,
-    //     dataType: "script"
-    // });    
-}
-
-function requestInfo() {
-    // jQuery.ajax({
-    //     url: "https://jira.intellective.com/s/b3827f4461d4436c6714c921c20d1f97-T/-mb7qmt/810001/ed30881a1c0940ba1231758ef6f853f8/4.0.1/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=c98c0312",
-    //     type: "get",
-    //     cache: true,
-    //     dataType: "script"
-    // });
-}
-
-function reportBug() {
-    // jQuery.ajax({
-    //     url: "https://jira.intellective.com/s/b3827f4461d4436c6714c921c20d1f97-T/-mb7qmt/810001/ed30881a1c0940ba1231758ef6f853f8/4.0.1/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=7a36e89a",
-    //     type: "get",
-    //     cache: true,
-    //     dataType: "script"
-    // });
-}
+window.ATL_JQ_PAGE_PROPS =  {
+    "ebed4e32": {
+        "triggerFunction": function(showCollectorDialog) {
+            jQuery("#request-enhancement-trigger").click(function(e) {
+                e.preventDefault();
+                showCollectorDialog();
+            });
+        }
+    },
+    "c98c0312": {
+        "triggerFunction": function(showCollectorDialog) {
+            jQuery("#request-info-trigger").click(function(e) {
+                e.preventDefault();
+                showCollectorDialog();
+            });
+        }
+    },
+    "7a36e89a": {
+        "triggerFunction": function(showCollectorDialog) {
+            jQuery("#report-bug-trigger").click(function(e) {
+                e.preventDefault();
+                showCollectorDialog();
+            });
+        }
+    }
+};
