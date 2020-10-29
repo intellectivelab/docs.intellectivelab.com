314d7bdb8d81d8428b07f270739f8c70928a404a
NKravchenko <nataliebayarea@gmail.com>
2020-10-29 20:32:44 +0100

    reverted changes

--- jekyll.bat
+++ jekyll.bat
@@ -1,2 +1,2 @@
 set PWD=%CD%
-docker run --rm --label=jekyll --volume=%PWD%:/srv/jekyll  -it -p 4001:4000 jekyll/jekyll:pages jekyll serve --livereload --force_polling %*
+docker run --rm --label=jekyll --volume=%PWD%:/srv/jekyll  -it -p 4000:4000 jekyll/jekyll:pages jekyll serve --livereload --force_polling %*
--- jekyll.sh
+++ jekyll.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --name=jekyll --label=jekyll --volume=$(pwd):/srv/jekyll -d -p 4001:4000 jekyll/jekyll:pages jekyll serve
+docker run --rm --name=jekyll --label=jekyll --volume=$(pwd):/srv/jekyll -d -p 4000:4000 jekyll/jekyll:pages jekyll serve
