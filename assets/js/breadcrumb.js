99388eea4eb6845ff536221e5ca02d933fc9b435
Shane Abe <sabe@intellective.com>
2020-11-04 10:26:43 -0500

    make last few crumbs clickable to go to page

--- assets/js/breadcrumb.js
+++ assets/js/breadcrumb.js
@@ -11,6 +11,16 @@ $(function() {
         return newKey;
     }
 
+    function buildHref(key) {
+        var url = document.URL,
+            index = url.indexOf(key);
+
+        if(index !== -1) {
+            index += key.length;
+            return url.slice(0, index);
+        }
+    }
+
     var container = $('#breadcrumbs')
         url = document.URL.match(/docs.*/g),
         crumbs = '<ol class="breadcrumb">';
@@ -18,14 +28,23 @@ $(function() {
     if(url && url.length > 0) {
         url = url[0];
 
-        var keys = url.split('/');
+        var keys = url.split('/'), crumb = "";
         keys.splice(0, 1);                        // Remove 'docs'
         keys.splice(keys.length-1, 1);            // Remove empty string at end
 
-        keys.forEach(key => {
-            key = key.replace(/-/g, " ");
-            key = caps(key);
-            crumbs += '<li class="breadcrumb-item"><a href="#">' + key + '</a></li>';
+        keys.forEach((key, i) => {
+            crumb = key.replace(/-/g, " ");
+            crumb = caps(crumb);
+            crumbs += '<li class="breadcrumb-item">';
+
+            if(i > 1) {
+                var href = buildHref(key);
+                crumbs += '<a href="' + href + '">' + crumb + '</a>';
+            } else {
+                crumbs += '<a class="a-nonclick" href="#">' + crumb + '</a>';
+            }
+
+            crumbs += '</li>';
         });
 
         crumbs += "</ol>";
